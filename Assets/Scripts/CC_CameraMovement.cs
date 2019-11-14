dc92fab6b02c0939cf229e1e24d31dc7ca9f6b21
Jaron Wilding <jaronwilding95@gmail.com>
2019-11-14 18:01:47 +0800

    Playermovement

---
+++ Assets/Scripts/CC_CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CC_CameraMovement : MonoBehaviour
+{
+    [SerializeField, Range(0, 10)] private float mouseSensitivity = 100f;
+
+    private Transform _playerMain = null;
+    private Transform _cameraOffset = null;
+    private float _rotation = 0f;
+
+    private void Start()
+    {
+        _cameraOffset = transform.parent.transform;
+        _playerMain = transform.parent.parent.transform;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        _rotation -= mouseY;
+        _rotation = Mathf.Clamp(_rotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(_rotation, 0f, 0f);
+        _playerMain.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/CC_PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CC_PlayerMovement : MonoBehaviour
+{
+    [SerializeField, Range(0,30)] private float _speed = 12f;
+    [SerializeField, Range(-15, 0)] private float _gravity = -9.81f;
+    [SerializeField, Range(1, 10)] private float _jumpHeight = 2.5f;
+
+    [SerializeField] private Transform _groundCheck = null;
+    [SerializeField, Range(0, 2)] private float _groundDist = 0.4f;
+    [SerializeField] private LayerMask _groundMask;
+    [SerializeField] private bool _showGizmo = false;
+
+    private CharacterController _charCon = null;
+    private Vector3 _velocity;
+    private bool _grounded;
+    
+
+    void Start()
+    {
+        _charCon = GetComponent<CharacterController>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(_showGizmo)
+        {
+            Gizmos.DrawWireSphere(_groundCheck.position, _groundDist);
+        }
+    }
+
+    void Update()
+    {
+        _grounded = Physics.CheckSphere(_groundCheck.position, _groundDist, _groundMask);
+
+        if(_grounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 fixedMovement = new Vector3(x, 0, z);
+        FixNormalize(ref fixedMovement);
+        
+        Vector3 movement = transform.right * fixedMovement.x + transform.forward * fixedMovement.z;
+
+        _charCon.Move(movement * _speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && _grounded)
+        {
+            _velocity.y += Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+        }
+
+        _velocity.y += _gravity * Time.deltaTime;
+
+        _charCon.Move(_velocity * Time.deltaTime);
+    }
+
+    private void FixNormalize(ref Vector3 movement)
+    {
+        Vector3 direction = movement.normalized;
+        Vector3 mainDirection = direction * Mathf.Clamp01(movement.magnitude / direction.magnitude);
+        movement = mainDirection;
+    }
+}
